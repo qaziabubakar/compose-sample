584c17fbc14c30ed693a10e5af584e0effbc3b32
Qazi Abubakar <qma_2007@yahoo.com>
2021-10-12 14:50:57 +0500

    Learned about Scaffold and state, TextField and how to define a coroutine scope

--- app/src/main/java/com/viewbinding/sampleapp/MainActivity.kt
+++ app/src/main/java/com/viewbinding/sampleapp/MainActivity.kt
@@ -3,43 +3,56 @@ package com.viewbinding.sampleapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Row(
-                Modifier
-                    .width(300.dp)
-                    .fillMaxHeight(0.7f)
-                    .background(Color.Green),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.SpaceEvenly
+            val scaffoldState = rememberScaffoldState()
+            var textState by remember {
+                mutableStateOf("")
+            }
+            val mScope = rememberCoroutineScope()
+
+            Scaffold(
+                modifier = Modifier.fillMaxSize(),
+                scaffoldState = scaffoldState
             ) {
-                Text("Some")
-                Text("New")
-                Text("Word")
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(horizontal = 16.dp),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    TextField(
+                        modifier = Modifier.fillMaxWidth(),
+                        value = textState,
+                        label = {
+                            Text(text = "Enter Name here!")
+                        },
+                        onValueChange = {
+                            textState = it
+                        })
+                    Spacer(modifier = Modifier.height(10.dp))
+                    Button(
+                        onClick = {
+                            mScope.launch {
+                                scaffoldState.snackbarHostState.showSnackbar("Hello $textState!")
+                            }
+                        }, modifier = Modifier.fillMaxWidth()
+                    ) {
+                        Text(text = "Click Me!")
+                    }
+                }
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    Greeting("Android Some")
 }
