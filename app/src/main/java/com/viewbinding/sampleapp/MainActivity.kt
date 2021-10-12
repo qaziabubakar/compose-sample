3f65c6236b6de4066bd2ff4b6af420bdef808fd4
Qazi Abubakar <qma_2007@yahoo.com>
2021-10-12 15:22:23 +0500

    Created lists using LazyColumn

--- app/src/main/java/com/viewbinding/sampleapp/MainActivity.kt
+++ app/src/main/java/com/viewbinding/sampleapp/MainActivity.kt
@@ -3,43 +3,70 @@ package com.viewbinding.sampleapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Row(
-                Modifier
-                    .width(300.dp)
-                    .fillMaxHeight(0.7f)
-                    .background(Color.Green),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.SpaceEvenly
-            ) {
-                Text("Some")
-                Text("New")
-                Text("Word")
+            Column(modifier = Modifier.fillMaxSize()) {
+                LazyColumn(
+                    modifier = Modifier.weight(1f)
+                ) {
+                    items(count = 500) {
+                        Text(
+                            text = "Item $it",
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(top = 10.dp),
+                            textAlign = TextAlign.Center,
+                            fontSize = 32.sp
+                        )
+                    }
+                }
+                LazyColumn(
+                    modifier = Modifier.weight(1f)
+                ) {
+                    itemsIndexed(
+                        listOf(
+                            "s",
+                            "sd",
+                            "sd",
+                            "s",
+                            "sd",
+                            "sd",
+                            "s",
+                            "sd",
+                            "sd",
+                            "s",
+                            "sd",
+                            "sd",
+                            "s",
+                            "sd",
+                            "sd"
+                        )
+                    ) { index, item ->
+                        Text(
+                            text = "$index $item",
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(top = 10.dp),
+                            textAlign = TextAlign.Center,
+                            fontSize = 32.sp
+                        )
+                    }
+                }
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    Greeting("Android Some")
 }
