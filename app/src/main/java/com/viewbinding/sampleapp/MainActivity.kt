0859b9340cb0741469a644d178b62bedc7d61b8e
Qazi Abubakar <qma_2007@yahoo.com>
2021-10-12 13:15:35 +0500

    Practiced on compose states

--- app/src/main/java/com/viewbinding/sampleapp/MainActivity.kt
+++ app/src/main/java/com/viewbinding/sampleapp/MainActivity.kt
@@ -4,42 +4,89 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
+import kotlin.random.Random
 
+/**
+ *
+ */
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Row(
-                Modifier
-                    .width(300.dp)
-                    .fillMaxHeight(0.7f)
-                    .background(Color.Green),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.SpaceEvenly
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .wrapContentSize(Alignment.Center)
             ) {
-                Text("Some")
-                Text("New")
-                Text("Word")
+                val color = remember {
+                    mutableStateOf(Color.Yellow)
+                }
+                val text = remember {
+                    mutableStateOf("1")
+                }
+                ColorBox(
+                    Modifier
+                        .weight(1f)
+                        .fillMaxSize(),
+                    {
+                        color.value = it
+                    },
+                    {
+                        text.value = it
+                    })
+                Box(
+                    Modifier
+                        .weight(1f)
+                        .fillMaxSize()
+                        .background(color.value),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        text = text.value, color = Color.Red,
+                        fontSize = 40.sp,
+                        textAlign = TextAlign.Center
+                    )
+                }
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    Greeting("Android Some")
+fun ColorBox(
+    modifier: Modifier = Modifier,
+    updateColor: (Color) -> Unit,
+    updateText: (String) -> Unit
+) {
+    Box(modifier = modifier
+        .background(color = Color.Red)
+        .clickable {
+            updateColor(
+                Color(
+                    Random.nextFloat(),
+                    Random.nextFloat(),
+                    Random.nextFloat(),
+                    1f
+                )
+            )
+            updateText(
+                Random
+                    .nextInt()
+                    .toString()
+            )
+        })
 }
